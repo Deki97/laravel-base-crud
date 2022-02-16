800b8a8f7965ecbc0e71e7af1f960da14da79d77
Deki97 <dejan97@libero.it>
2022-02-16 15:24:14 +0100

    first commit

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics_array = config('comics');
+
+        dd($comics_array);
+    }
+}
