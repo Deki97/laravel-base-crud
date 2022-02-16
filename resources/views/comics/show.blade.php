6d201fcaf9de7f09c6cc58ecfcd99a32215e3840
Deki97 <dejan97@libero.it>
2022-02-16 16:46:19 +0100

    show done

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h1>Titolo comic</h1>
+
+    <div class="card">
+        <img class="card-img-top" src="{{ $comic->thumb }}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">{{ $comic->title }}</h5>
+          <div>Price: {{ $comic->price }} </div>
+          <div>Series: {{ $comic->series }} </div>
+          <div>On Sale: {{ $comic->sale_date }} </div>
+          <p class="card-text">{{ $comic->description }} </p>
+        </div>
+      </div>
 @endsection
