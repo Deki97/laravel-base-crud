ac92c9a33b9e328ad9df55e58fae154f96a69611
Deki97 <dejan97@libero.it>
2022-02-16 16:01:15 +0100

    created layout folder with app

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
 
+@section('main_content')
     <h1>List of Comics</h1>
 
     @foreach ($comics as $comic)
@@ -17,5 +10,4 @@
             <div>Price: {{ $comic->price }}</div>
         </div>
     @endforeach
-</body>
-</html>
+@endsection
